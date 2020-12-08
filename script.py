508ae73f03c2073460518e65bff04cec86aaadfa
Luketss <lucassilluzio@gmail.com>
2020-12-08 20:50:33 -0300

    plot data wt test of interval of time for dataframe

--- script.py
+++ script.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 
-import pandas as import pd
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-plt.style.use(['dark_background', 'presentation'])
+plt.style.use('fivethirtyeight')
 
 DT_FILE_PATH = 'bitstamp.csv'
 
-DATA_INICIO = '2010-10-10'
-DATA_FIM = '2020-10-10'
+DATA_INICIO = '2000-10-10'
+DATA_FIM = '2010-10-10'
 
-def getDataSetFile():
-    pass
+def getDataSetFile(file_path):
+    dt_bitstamp = pd.read_csv(file_path)
+    print(dt_bitstamp.head())
+    dt_bitstamp = pd.DataFrame(dt_bitstamp, index=pd.date_range(DATA_INICIO, DATA_FIM, freq='H').values)
+    #dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
+    print(dt_bitstamp.head())
+
+    return dt_bitstamp
+    #print(df['Timestamp'].loc[DATA_INICIO:DATA_FIM])
 
 
 def movingAverage(dataSet, period=9):
@@ -22,11 +29,17 @@ def bollingerBands(dataSet, period=14, deviation=2):
     pass
 
 def plotBollingerAndMAverage(column_list):
-    pass
+    dataSet[show_list].plot(figsize=(12.2, 6.4))
+    plt.title('Close Price')
+    plt.xlabel('Close Price')
+    plt.ylabel('Price')
+    plt.show()
 
 
 def main():
-    pass
+    dataSet = getDataSetFile(DT_FILE_PATH)
+
+    dataSet.to_csv('teste.csv')
 
 if __name__ == '__main__':
     main()
