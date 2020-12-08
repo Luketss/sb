740954ec70771a47cfaee9d55bfd8f486942e09b
Luketss <lucassilluzio@gmail.com>
2020-12-08 20:06:03 -0300

    define basic model for project

---
+++ script.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+import pandas as import pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use(['dark_background', 'presentation'])
+
+DT_FILE_PATH = 'bitstamp.csv'
+
+DATA_INICIO = '2010-10-10'
+DATA_FIM = '2020-10-10'
+
+def getDataSetFile():
+    pass
+
+
+def movingAverage(dataSet, period=9):
+    pass
+
+
+def bollingerBands(dataSet, period=14, deviation=2):
+    pass
+
+def plotBollingerAndMAverage(column_list):
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
