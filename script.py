2a9d81112fa02e0572bd79865a2de8b01833d74a
Luketss <lucassilluzio@gmail.com>
2020-12-09 12:57:58 -0300

    version without data filter - with bb and ema

--- script.py
+++ script.py
@@ -7,43 +7,44 @@ plt.style.use('fivethirtyeight')
 
 DT_FILE_PATH = 'bitstamp.csv'
 
-DATA_INICIO = '2000-10-10'
-DATA_FIM = '2010-10-10'
+DATA_INICIO = '2018-10-10'
+DATA_FIM = '2020-10-10'
 
 def getDataSetFile(file_path):
     dt_bitstamp = pd.read_csv(file_path)
+    dt_bitstamp.set_index('Timestamp')
+    #dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
+    #dt_bitstamp = pd.read_csv(file_path, iterator=True, chunksize=10000)
+    # dt_bitstamp = pd.concat(dt_bitstamp, ignore_index=True)
+    # dt_bitstamp = pd.DataFrame(dt_bitstamp, index=pd.date_range(DATA_INICIO, DATA_FIM).values)
 
-    print(dt_bitstamp.head())
+    return dt_bitstamp
 
-    dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
+    #dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
     #dt_bitstamp = pd.DataFrame(dt_bitstamp, index=pd.date_range(DATA_INICIO, DATA_FIM).values)  
     #dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
-    print(dt_bitstamp.head())
 
     # a = pd.to_datetime(dt_bitstamp['Timestamp'], unit='s').dt_bitstamp.strftime('%Y-%m-%d %H:%M')
 
     # dt_bitstamp['Timestamp'] = pd.date_range(DATA_INICIO, DATA_FIM, freq='H').values
     # df2 = dt_bitstamp.set_index('Timestamp')
 
-    return dt_bitstamp
     #print(df['Timestamp'].loc[DATA_INICIO:DATA_FIM])
 
 
 def movingAverage(dataSet, period=9):
-    ema = dataSet['Close'].ewm(span=period, adjust=False).mean()
-
-    return ema
+    dataSet['EMA'] = dataSet['Close'].ewm(span=period, adjust=False).mean()
 
 
 def bollingerBands(dataSet, period=14, deviation=2):
-    dataSet['BBMiddle'] = dataSet['Close'].rolling(window=period).mean()
+    dataSet['MA'] = dataSet['Close'].rolling(window=period).mean()
 
     dataSet['STD'] = dataSet['Close'].rolling(window=deviation).std()
 
-    dataSet['BBUpper'] = dataSet['BBMiddle'] + (dataSet['STD'] * 2)
-    dataSet['BBLower'] = dataSet['BBMiddle'] - (dataSet['STD'] * 2)
+    dataSet['BBUpper'] = dataSet['MA'] + (dataSet['STD'] * 2)
+    dataSet['BBLower'] = dataSet['MA'] - (dataSet['STD'] * 2)
 
-    column_list = ['BBMiddle', 'BBUpper', 'BBLower']
+    column_list = ['MA', 'BBUpper', 'BBLower']
     return column_list
 
 def plotBollingerAndMAverage(dataSet, column_list):
@@ -55,18 +56,23 @@ def plotBollingerAndMAverage(dataSet, column_list):
 
 
 def writeDataFrameToCSV(dataSet, column_list):
-    finalDataFrame = dataSet[column_list]
+    finalDataFrame = dataSet[['Timestamp', 'MA', 'BBUpper', 'BBLower','EMA']]
+    print(finalDataFrame.head())
+
+    finalDataFrame = finalDataFrame.set_index(pd.DatetimeIndex(finalDataFrame['Timestamp'].values))
+    print(finalDataFrame.head())
 
     finalDataFrame.to_csv('teste.csv')
 
+
 def main():
     dataSet = getDataSetFile(DT_FILE_PATH)
-    bands = bollingerBands(dataSet)
-    bands.append('Close')
-    plotBollingerAndMAverage(dataSet, bands)
-    #writeDataFrameToCSV(dataSet, bands)
+    dataSet.head()
+    movingAverage(dataSet)
+    bands = bollingerBands(dataSet,2)
+    # plotBollingerAndMAverage(dataSet, bands)
+    writeDataFrameToCSV(dataSet, bands)
 
-    #dataSet.to_csv('teste.csv')
 
 if __name__ == '__main__':
     main()
