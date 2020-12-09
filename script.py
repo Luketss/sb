6ab6fe6eb9782de0d65d3577b6335942e5909081
Luketss <lucassilluzio@gmail.com>
2020-12-09 11:15:21 -0300

    add writeDataToCSV function

--- script.py
+++ script.py
@@ -13,9 +13,17 @@ DATA_FIM = '2010-10-10'
 def getDataSetFile(file_path):
     dt_bitstamp = pd.read_csv(file_path)
     print(dt_bitstamp.head())
-    dt_bitstamp = pd.DataFrame(dt_bitstamp, index=pd.date_range(DATA_INICIO, DATA_FIM, freq='H').values)
+    dt_bitstamp = pd.read_csv(file_path)
+
+    dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
+    #dt_bitstamp = pd.DataFrame(dt_bitstamp, index=pd.date_range(DATA_INICIO, DATA_FIM, freq='H').values)
     #dt_bitstamp = dt_bitstamp.set_index(pd.DatetimeIndex(dt_bitstamp['Timestamp'].values))
-    print(dt_bitstamp.head())
+    #print(dt_bitstamp.head())
+
+    # a = pd.to_datetime(dt_bitstamp['Timestamp'], unit='s').dt_bitstamp.strftime('%Y-%m-%d %H:%M')
+
+    # dt_bitstamp['Timestamp'] = pd.date_range(DATA_INICIO, DATA_FIM, freq='H').values
+    # df2 = dt_bitstamp.set_index('Timestamp')
 
     return dt_bitstamp
     #print(df['Timestamp'].loc[DATA_INICIO:DATA_FIM])
@@ -46,10 +54,17 @@ def plotBollingerAndMAverage(column_list):
     plt.show()
 
 
+def writeDataFrameToCSV(dataSet, column_list):
+    finalDataFrame = dataSet[column_list]
+
+    finalDataFrame.to_csv('teste.csv')
+
 def main():
     dataSet = getDataSetFile(DT_FILE_PATH)
+    a = bollingerBands(dataSet)
+    writeDataFrameToCSV(dataSet, a)
 
-    dataSet.to_csv('teste.csv')
+    #dataSet.to_csv('teste.csv')
 
 if __name__ == '__main__':
     main()
